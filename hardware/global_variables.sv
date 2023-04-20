430aa509dca1f9aa22f74a3e9a716b44f870b4f3
Alex Yu <alex.yu0753@gmail.com>
2023-04-20 14:44:57 -0400

    added global variables

---
+++ hardware/global_variables.sv
@@ -0,0 +1,5 @@
+// Audio Codec Macros
+`define AUDIO_IN_GAIN 9'h010
+`define AUDIO_OUT_GAIN 9'h061
+
+`define SAMPLE_RATE_CNTRL 9'd0  //No particularly helpful, but see page 44 of datasheet of more info: https://statics.cirrus.com/pubs/proDatasheet/WM8731_v4.9.pdf
