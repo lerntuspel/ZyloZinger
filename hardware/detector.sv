7df90d31a019ff47586613ee19ca8ee25191759e
Sienna Brent <scb2197@columbia.edu>
2023-04-21 13:53:10 -0400

    Update detector.sv

--- hardware/detector.sv
+++ hardware/detector.sv
@@ -1,23 +1,25 @@
 //takes in 4 power inputs and outputs either "silence" or one of the 4 bins
-`define INTERVAL 10
-`define INTERVAL_WIDTH 4
-`define SHIFT 8
+`define INTERVAL 15
+`define INTERVAL_WIDTH 5
+`define SHIFT 8 //how much greater the signal we're looking for has to be than the other signals
 
-module detector(
+`define GREATER_THAN 8
 
+module detector #(
+    parameter ARR_WIDTH=16
+)
+(
 input clk,
 input reset,
-input advance1, advance2, advance3, advance4,
+input advance,
 input logic signed [63:0] power_1,
 input logic signed [63:0] power_2,
 input logic signed [63:0] power_3,
 input logic signed [63:0] power_4,
 output logic [2:0] result, //00 for silence, 1-4 for powers 1-4 respectively
-output logic [2:0] overall_result,
-output logic flag
-
+output logic [2:0] overall_result
 );
-    // logic flag;
+    logic flag;
 
     logic ratio_12;
     logic ratio_13;
@@ -31,6 +33,7 @@ output logic flag
     logic ratio_41;
     logic ratio_42;
     logic ratio_43;
+    logic [2:0] last_bin = 3'b111;
 
     logic [`INTERVAL_WIDTH-1:0] pw1_count = 0;
     logic [`INTERVAL_WIDTH-1:0] pw2_count = 0;
@@ -45,9 +48,9 @@ output logic flag
     logic pw3_count_greatest;
     logic pw4_count_greatest;
     logic silence_count_greatest;
-    
-    logic advance;
-	
+
+    logic [2:0] result_array [$:ARR_WIDTH];
+
 always @(posedge clk) begin
 
     flag <= advance;
@@ -69,34 +72,43 @@ always @(posedge clk) begin
     end
 
     if(flag)begin
-        
-        case(result) 
-            3'b00: silence_count <= silence_count + 1;
-            3'b01: pw1_count <= pw1_count + 1;
-            3'b10: pw2_count <= pw2_count + 1;
-            3'b11: pw3_count <= pw3_count + 1;
-            3'b100: pw4_count <= pw4_count + 1;
-        
-        endcase
-    end
 
-    if (overall_count == `INTERVAL)begin
-        if (pw1_count_greatest)
-            overall_result <= 3'b01;
-        else if (pw2_count_greatest)
-            overall_result <= 3'b10;
-        else if (pw3_count_greatest)
-            overall_result <= 3'b11;
-        else if (pw4_count_greatest)
-            overall_result <= 3'b100;
-        else if (silence_count_greatest)
-            overall_result <= 3'b00;
+        result_array.push_front(result);
+        if(result_array.size() == ARR_WIDTH ) begin
+            last_bin = result_array.pop_back();
 
-        pw1_count <= 0;
-        pw2_count <= 0;
-        pw3_count <= 0;
-        pw4_count <= 0;
-        silence_count <= 0;
+            if (pw1_count >= `GREATER_THAN)
+                overall_result <= 3'b01;
+            else if (pw2_count >= `GREATER_THAN)
+                overall_result <= 3'b10;
+            else if (pw3_count >= `GREATER_THAN)
+                overall_result <= 3'b11;
+            else if (pw4_count >= `GREATER_THAN)
+                overall_result <= 3'b100;
+            else
+                overall_result <= 3'b00;
+        end
+
+        if(last_bin != result) begin
+
+            case(result) 
+                3'b00:  silence_count <= silence_count + 1;
+                3'b01:  pw1_count <= pw1_count + 1;
+                3'b10:  pw2_count <= pw2_count + 1;
+                3'b11:  pw3_count <= pw3_count + 1;
+                3'b100: pw4_count <= pw4_count + 1;        
+            endcase
+
+            case(last_bin) 
+                3'b00:  silence_count <= silence_count - 1;
+                3'b01:  pw1_count <= pw1_count - 1;
+                3'b10:  pw2_count <= pw2_count - 1;
+                3'b11:  pw3_count <= pw3_count - 1;
+                3'b100: pw4_count <= pw4_count - 1;
+            endcase
+
+        end
+
     end
         
 
@@ -116,11 +128,11 @@ end
     assign ratio_43 = power_4 /`SHIFT > power_3;
 
     assign overall_count = pw1_count + pw2_count + pw3_count + pw4_count + silence_count;
+    /*
     assign pw1_count_greatest = (pw1_count >= pw2_count) && (pw1_count >= pw3_count) && (pw1_count >= pw4_count) && (pw1_count >= silence_count);
     assign pw2_count_greatest = (pw2_count >= pw1_count) && (pw2_count >= pw3_count) && (pw2_count >= pw4_count) && (pw1_count >= silence_count);
     assign pw3_count_greatest = (pw3_count >= pw1_count) && (pw3_count >= pw2_count) && (pw3_count >= pw4_count) && (pw1_count >= silence_count);
     assign pw4_count_greatest = (pw4_count >= pw1_count) && (pw4_count >= pw2_count) && (pw4_count >= pw3_count) && (pw1_count >= silence_count);
     assign silence_count_greatest = (silence_count >= pw1_count) && (silence_count >= pw2_count) && (silence_count >= pw3_count) && (silence_count >= pw4_count);
-    assign advance = (advance1||advance2||advance3||advance4);
-
+    */
 endmodule
